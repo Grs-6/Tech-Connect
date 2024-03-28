6596d6b996d8a6db1fc2166a189343824860029b
Gayathre_6 <gayathre.ranga@ei.study>
2024-03-28 20:55:51 +0530

    added memento and template design patterns

---
+++ MementoDP/TextMemento.java
@@ -0,0 +1,13 @@
+package MementoDP;
+
+public class TextMemento {
+    private final String state;
+
+    public TextMemento(String state) {
+        this.state = state;
+    }
+
+    public String getState() {
+        return state;
+    }
+}
---
+++ TemplateDP/CoffeeMaker.java
@@ -0,0 +1,13 @@
+package TemplateDP;
+class CoffeeMaker extends BeverageMaker {
+    // Implementing abstract methods
+    @Override
+    void brew() {
+        System.out.println("Dripping coffee through filter");
+    }
+ 
+    @Override
+    void addCondiments() {
+        System.out.println("Adding sugar and milk");
+    }
+}
